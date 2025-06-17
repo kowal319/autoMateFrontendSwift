c16518753626283856d09c9a9d79ae1259a8a16f
kowal319 <bartlomiej319@wp.pl>
2025-06-17 21:38:56 +0200

    Initial Commit

---
+++ AutoMate/AutoMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMateApp.swift
+//  AutoMate
+//
+//  Created by Bartlomiej Kowalczyk on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AutoMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoMateTests/AutoMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoMateTests.swift
+//  AutoMateTests
+//
+//  Created by Bartlomiej Kowalczyk on 17/06/2025.
+//
+
+import Testing
+@testable import AutoMate
+
+struct AutoMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
